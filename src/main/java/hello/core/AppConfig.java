5a3e5f6bd841527c78dae6d560b081f497ca6851
kim_ki_yun <kiyoon9390@gmail.com>
2022-03-25 00:43:45 +0900

    feat:spring Container 적용

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,25 +8,35 @@ import hello.core.member.MemberService;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderSerivce;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService(){
 
         return new MemberSerivceImpl(memberRepository());
     }// MemoryMemeberRepository를 생성하고 그 참조값을 MemberSErviceImpl에 전달 -> 주입
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean // option으로 name 으로 bean key 값 설정 가능
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository(); // 만약 repository를 바꾼다면 해당 코드 부분만 바꾸면 된다
     }
 
-
+    @Bean
     public OrderSerivce orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
+
+    // 구성 영역에만 있는 코드들만 손대면 된다
+    // client 코드를 전혀 변경하지 않고도 application 기능을 확장 가능
+
 }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,20 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberSerivceImpl;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args){
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class); // annotaiion 기반으로 bean 인 것들을 다 spring conainer에 넣어준다
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        // key는 memberService고 value는 MemberService로 해서 container에 등록
+        // 기존과 다른점은 log가 나온다 singleton bean
+
         Member member = new Member(1L, "memberA", Grade.VIP);
 
         memberService.join(member);
